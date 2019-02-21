62916e4314b1bc70b9095a75a85e4e44055f72bd
vironcr <47869414+vironcr@users.noreply.github.com>
2019-02-21 20:45:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <h1>Hello world</h1>
 
-<?php
-
-$ip = $_SERVER['REMOTE_ADDR']; //get the visitor's IP
+ <?php
+ $ip = $_SERVER['REMOTE_ADDR']; //get the visitor's IP
+ $country = file_get_contents('http://ipinfo.io/'.$ip); //fetch the country
 $date = date(DATE_RFC822);
-$data = "$date $ip\n"; //format a log entry
-
-file_put_contents('./log.txt', $data, FILE_APPEND | LOCK_EX); //write to the log file
-if ($_GET["clean"] == "1") {
- unlink("./log.txt") 
-}
-?>
+$country = json_decode($country, true)["country"]; //turn the IP string (API) into an array
+$data = "<tr><td>".$date."</td><td>".$ip."</td><td>".$country."</td></tr>"; //format a log entry
+ file_put_contents('./log.txt', $data, FILE_APPEND | LOCK_EX); //write to the log file
+ ?>
